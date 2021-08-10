1c6c478ed30cf42cb03fe085e1eb190df0a883cc
Kundan Kumar Gupta <kundankumargupta@Kundans-MacBook-Air.local>
2021-08-10 08:55:20 +0530

    cpp Solution

--- A_Insomnia_cure.cpp
+++ A_Insomnia_cure.cpp
@@ -1,6 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-
+    int k,l,m,n,d;
+    cin>>k>>l>>m>>n>>d;
+    vector<bool> v(d+1,false);
+    for(int i=k;i <=d;i+=k ){
+        v[i]=true;
+    }
+    for(int i=l;i<=d;i+=l){
+        v[i]=true;
+    }
+    for(int i=m;i<=d;i+=m){
+        v[i]=true;
+    }
+    for(int i=n;i<=d;i+=n){
+        v[i]=true;
+    }
+    int ans=0;
+    for(int i=1;i<=d;i++){
+        if(v[i]==true)
+            ans++;
+    }
+    cout<<ans<<endl;
 return 0;
 }
